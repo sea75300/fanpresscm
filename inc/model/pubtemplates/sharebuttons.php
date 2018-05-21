394fb0d2bec0f50c06b3a61082414cf1179c079b
stefan <stefan@stefan-laptop>
2018-05-21 22:02:02 +0200

    WhatsApp Share Button

--- inc/model/pubtemplates/sharebuttons.php
+++ inc/model/pubtemplates/sharebuttons.php
@@ -30,6 +30,7 @@ final class sharebuttons extends template {
         '{{tumblr}}' => '',
         '{{pinterest}}' => '',
         '{{reddit}}' => '',
+        '{{whatsapp}}' => '',
         '{{email}}' => '',
         '{{link}}' => '',
         '{{description}}' => '',
@@ -72,49 +73,8 @@ final class sharebuttons extends template {
             return '';
         }
 
-        $tags = $this->events->trigger('pub\parseShareButtons', array_merge($this->replacementInternal, [
-            '{{facebook}}' => [
-                'link' => "https://www.facebook.com/sharer/sharer.php?u={$this->link}&amp;t={$this->description}",
-                'icon' => "default/facebook.png",
-                'text' => "Facebook",
-            ],
-            '{{twitter}}' => [
-                'link' => "https://twitter.com/intent/tweet?source={$this->link}&amp;text={$this->description}",
-                'icon' => "default/twitter.png",
-                'text' => "Twitter",
-            ],
-            '{{googlePlus}}' => [
-                'link' => "https://plus.google.com/share?url={$this->link}",
-                'icon' => "default/googleplus.png",
-                'text' => "Google+",
-            ],
-            '{{tumblr}}' => [
-                'link' => "http://www.tumblr.com/share?v=3&amp;u={$this->link}&amp;t={$this->description}&amp;s=",
-                'icon' => "default/tumblr.png",
-                'text' => "Share on Tumblr",
-            ],
-            '{{pinterest}}' => [
-                'link' => "http://pinterest.com/pin/create/button/?url={$this->link}&amp;description={$this->description}",
-                'icon' => "default/pinterest.png",
-                'text' => "Pin it",
-            ],
-            '{{reddit}}' => [
-                'link' => "http://www.reddit.com/submit?url={$this->link}&amp;title={$this->description}",
-                'icon' => "default/reddit.png",
-                'text' => "Submit to Reddit",
-            ],
-            '{{email}}' => [
-                'link' => "mailto:?subject={$this->description}&amp;body={$this->link}",
-                'icon' => "default/email.png",
-                'text' => "Share via E-Mail",
-            ],
-            '{{link}}' => $this->link,
-            '{{description}}' => $this->description,
-            '{{credits}}' => "<!-- default button icon set powered by http://simplesharingbuttons.com/ -->"
-        ]));
-
         $content = "<!-- Start FanPress CM Share Buttons -->".PHP_EOL.$this->content.PHP_EOL."<!-- Stop FanPress CM Share Buttons -->";
-        foreach ($tags as $replacement => $value) {
+        foreach ($this->initTags() as $replacement => $value) {
             
             if (!is_array($value)) {
                 $content = str_replace($replacement, $value, $content);
@@ -122,7 +82,15 @@ final class sharebuttons extends template {
             }
 
             $value['icon'] = \fpcm\classes\dirs::getDataUrl(\fpcm\classes\dirs::DATA_SHARE, $value['icon']);
-            $content = str_replace($replacement, "<a href=\"{$value['link']}\"><img src=\"{$value['icon']}\" alt=\"{$value['text']}\"></a>", $content);
+
+            $dataStr = '';
+            if (count($value['data'])) {
+                foreach ($value['data'] as $key => $dataValue) {
+                    $dataStr .= 'data-'.$key.'="'.$dataValue.'"';
+                }
+            }
+
+            $content = str_replace($replacement, "<a href=\"{$value['link']}\" {$dataStr}><img src=\"{$value['icon']}\" alt=\"{$value['text']}\"></a>", $content);
         }
 
         return $content;
@@ -139,6 +107,69 @@ final class sharebuttons extends template {
         $this->link = rawurlencode($link);
         $this->description = $description;
         return true;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+    private function initTags() : array
+    {
+        return $this->events->trigger('pub\parseShareButtons', array_merge($this->replacementInternal, [
+            '{{facebook}}' => [
+                'link' => "https://www.facebook.com/sharer/sharer.php?u={$this->link}&amp;t={$this->description}",
+                'icon' => "default/facebook.png",
+                'text' => "Facebook",
+                'data' => []
+            ],
+            '{{twitter}}' => [
+                'link' => "https://twitter.com/intent/tweet?source={$this->link}&amp;text={$this->description}",
+                'icon' => "default/twitter.png",
+                'text' => "Twitter",
+                'data' => []
+            ],
+            '{{googlePlus}}' => [
+                'link' => "https://plus.google.com/share?url={$this->link}",
+                'icon' => "default/googleplus.png",
+                'text' => "Google+",
+                'data' => []
+            ],
+            '{{tumblr}}' => [
+                'link' => "http://www.tumblr.com/share?v=3&amp;u={$this->link}&amp;t={$this->description}&amp;s=",
+                'icon' => "default/tumblr.png",
+                'text' => "Share on Tumblr",
+                'data' => []
+            ],
+            '{{pinterest}}' => [
+                'link' => "http://pinterest.com/pin/create/button/?url={$this->link}&amp;description={$this->description}",
+                'icon' => "default/pinterest.png",
+                'text' => "Pin it",
+                'data' => []
+            ],
+            '{{reddit}}' => [
+                'link' => "http://www.reddit.com/submit?url={$this->link}&amp;title={$this->description}",
+                'icon' => "default/reddit.png",
+                'text' => "Submit to Reddit",
+                'data' => []
+            ],
+            '{{whatsapp}}' => [
+                'link' => "whatsapp://send?text={$this->description}: {$this->link}",
+                'icon' => "default/whatsapp.png",
+                'text' => "Share on WhatsApp",
+                'data' => [
+                    'action' => 'share/whatsapp/share'
+                ]
+            ],
+            '{{email}}' => [
+                'link' => "mailto:?subject={$this->description}&amp;body={$this->link}",
+                'icon' => "default/email.png",
+                'text' => "Share via E-Mail",
+                'data' => []
+            ],
+            '{{link}}' => $this->link,
+            '{{description}}' => $this->description,
+            '{{credits}}' => "<!-- Icon set powered by http://simplesharingbuttons.com and https://whatsappbrand.com/ -->"
+        ]));
     }
 
 }
