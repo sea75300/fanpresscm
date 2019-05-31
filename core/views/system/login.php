01c605cdc5f4f024b2a3886fa23ab376f6428af7
stefan <stefan@stefan-pc>
2019-05-31 20:29:28 +0200

    Logi: small display style fix

--- core/views/system/login.php
+++ core/views/system/login.php
@@ -1,5 +1,5 @@
 <?php /* @var $theView \fpcm\view\viewVars */ ?>
-<div class="row no-gutters fpcm-ui-full-view-height">
+<div class="row no-gutters fpcm-ui-full-view-height m-2 ms-0">
     <div class="fpcm-ui-form-login col-12 col-sm-10 col-md-8 col-lg-5 fpcm-ui-margin-center align-self-center">
         <div class="fpcm-ui-background-white-50p fpcm-ui-border-radius-all fpcm-ui-border-blue-light fpcm-ui-padding-md-tb">
 
