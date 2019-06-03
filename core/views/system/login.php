264414d3892400c7e38b0b075de689a9126f4286
stefan <stefan@stefan-laptop>
2019-06-03 21:38:22 +0200

    Login-Form-Stylefixes

--- core/views/system/login.php
+++ core/views/system/login.php
@@ -1,17 +1,17 @@
 <?php /* @var $theView \fpcm\view\viewVars */ ?>
 <div class="row no-gutters fpcm-ui-full-view-height m-2 ms-0">
     <div class="fpcm-ui-form-login col-12 col-sm-10 col-md-8 col-lg-5 fpcm-ui-margin-center align-self-center">
-        <div class="fpcm-ui-background-white-50p fpcm-ui-border-radius-all fpcm-ui-border-blue-light fpcm-ui-padding-md-tb">
+        <div class="fpcm-ui-background-white-50p fpcm-ui-border-radius-all fpcm-ui-border-blue-light fpcm-ui-padding-md-tb fpcm-ui-padding-lg-lr">
 
             <h1 class="fpcm-ui-margin-md-bottom"><?php $theView->icon('chevron-right'); ?> <span>FanPress CM</span> <span>News System</span></h1>
             
-            <div class="row fpcm-ui-padding-md-tb">
+            <div class="row no-gutters fpcm-ui-padding-md-tb">
                 <div class="col-12">
                     <?php $theView->textInput($userNameField)->setText('GLOBAL_USERNAME')->setPlaceholder(true)->setAutocomplete(false)->setAutoFocused(true); ?>
                 </div>
             </div>
 
-            <div class="row fpcm-ui-padding-md-tb">
+            <div class="row no-gutters fpcm-ui-padding-md-tb">
                 <div class="col-12">
                 <?php if ($resetPasswort) : ?>
                     <?php $theView->textInput('email')->setText('GLOBAL_EMAIL')->setPlaceholder(true)->setAutocomplete(false); ?>
@@ -22,7 +22,7 @@
             </div>
             
             <?php if ($twoFactorAuth) : ?>
-            <div class="row fpcm-ui-padding-md-tb fpcm-ui-hidden" id="fpcm-loginauthcode-box">
+            <div class="row no-gutters fpcm-ui-padding-md-tb fpcm-ui-hidden" id="fpcm-loginauthcode-box">
                 <div class="col-12">
                 <?php $theView->textInput('login[authcode]')->setText('LOGIN_AUTHCODE')->setPlaceholder(true)->setAutocomplete(false); ?>
                 </div>
@@ -30,12 +30,14 @@
             <?php endif; ?>
 
             <?php if ($resetPasswort) : ?>
-            <div class="row fpcm-ui-padding-md-tb">
-                <div class="col-6">
-                    <?php print $captcha->createPluginText(); ?>
-                </div>
-                <div class="col-6">
-                    <?php print $captcha->createPluginInput(true); ?>
+            <div class="row no-gutters fpcm-ui-padding-md-tb">
+                <label class="col-6 fpcm-ui-field-label-general align-self-center">
+                    <div class="fpcm-ui-padding-md-lr"><?php print $captcha->createPluginText(); ?></div>
+                </label>
+                <div class="col-6 fpcm-ui-input-wrapper fpcm-ui-padding-none-lr fpcm-ui-border-radius-right">
+                    <div class="fpcm-ui-input-wrapper-inner">
+                        <?php print $captcha->createPluginInput(false); ?>
+                    </div>
                 </div>
             </div>
             <?php endif; ?>
