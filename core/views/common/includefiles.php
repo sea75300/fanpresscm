3694aada725729404792e87986e4aadb03e762f2
stefan <stefan@stefan-pc>
2019-09-28 15:00:45 +0200

    * Prefetch for JS and CSS data * Update Raleway font

--- core/views/common/includefiles.php
+++ core/views/common/includefiles.php
@@ -1,12 +1,12 @@
 <?php foreach ($theView->filesCss as $cssFile) : ?>
 <?php if (is_array($cssFile)) : ?>
-    <?php print $cssFile[0]; ?><link rel="stylesheet" type="text/css" href="<?php print $cssFile[1]; ?>"><?php print $cssFile[2]; ?>
+    <?php print $cssFile[0]; ?><link rel="stylesheet" type="text/css" href="<?php print $cssFile[1]; ?>" rel="prefetch"><?php print $cssFile[2]; ?>
 <?php else : ?>
-    <link rel="stylesheet" type="text/css" href="<?php print $cssFile; ?>">
+    <link rel="stylesheet" type="text/css" href="<?php print $cssFile; ?>" rel="prefetch">
 <?php endif; ?>
 <?php endforeach; ?>
 
 <?php foreach ($theView->filesJs as $jsFile) : ?>
     <?php if (!trim($jsFile)) continue; ?>
-    <script src="<?php print $jsFile; ?>"></script>
+    <script src="<?php print $jsFile; ?>" rel="prefetch"></script>
 <?php endforeach; ?>
