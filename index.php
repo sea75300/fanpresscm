90f485f5c5f95e6ead67c001eecb5515df251612
stefan <stefan@stefan-pc>
2020-04-03 13:36:21 +0200

    Release FPCM 4.4.0-b4

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * FanPress CM main index file
  * 
  * @author Stefan Seehafer <sea75300@yahoo.de>
- * @copyright (c) 2011-2018, Stefan Seehafer
+ * @copyright (c) 2011-2020, Stefan Seehafer
  * @license http://www.gnu.org/licenses/gpl.txt GPLv3
  */
 header('X-XSS-Protection: 1; mode=block');
@@ -12,6 +12,5 @@ header('X-XSS-Protection: 1; mode=block');
 require_once __DIR__ . '/inc/controller/main.php';
 require_once __DIR__ . '/inc/common.php';
 
-$mainController = new fpcm\controller\main();
-$mainController->exec();
+(new fpcm\controller\main)->exec();
 ?>
