ae42511308544247e324ac43ff9c591c06867615
stefan <stefan@stefan-pc>
2021-01-21 19:54:37 +0100

    Style bugxies and templatre cleanup

--- core/views/articles/massedit.php
+++ core/views/articles/massedit.php
@@ -2,7 +2,7 @@
 <div class="fpcm-ui-dialog-layer fpcm-ui-hidden fpcm-massedit-dialog" id="fpcm-dialog-articles-massedit">   
     <?php fpcm\components\components::getMassEditFields($masseditFields); ?>
     
-    <div class="row fpcm-ui-padding-md-tb">
+    <div class="row py-2">
         <label class="col-12 col-md-4 fpcm-ui-field-label-general">
             <?php $theView->icon('tags')->setSize('lg'); ?>
             <?php $theView->write('TEMPLATE_ARTICLE_CATEGORYTEXTS'); ?>
