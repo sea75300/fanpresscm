8b17f90a2587ab67adbe108ac2d94584dbe5b436
stefan <stefan@stefan-pc>
2021-10-20 22:38:53 +0200

    Update fav icon

--- core/views/common/header.php
+++ core/views/common/header.php
@@ -7,6 +7,7 @@
         <meta name="robots" content="noindex, nofollow">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="apple-touch-icon" sizes="180x180" href="<?php print $theView->themePath; ?>apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="<?php print $theView->themePath; ?>android-icon-192x192.png">
         <link rel="icon" type="image/png" sizes="32x32" href="<?php print $theView->themePath; ?>favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="<?php print $theView->themePath; ?>favicon-16x16.png">        
         <link rel="shortcut icon" href="<?php print $theView->themePath; ?>favicon.ico" />
