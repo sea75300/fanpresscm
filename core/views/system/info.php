d012fa2fd3d8fdd1cff60ca693e03de9a15246ff
stefan <stefan@stefan-pc>
2022-03-18 21:55:33 +0100

    Style update info view

--- core/views/system/info.php
+++ core/views/system/info.php
@@ -1,21 +1,14 @@
 <?php /* @var $theView fpcm\view\viewVars */ ?>
 <div class="border-5 border-top border-primary">
-    <div class="row">
-        <div class="col-12">
-            <h3 class="my-3"><?php $theView->write('SYSTEM_HL_OPTIONS_GENERAL'); ?></h3>
-            <?php print $content; ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <h3 class="mb-3"><?php $theView->write('VERSION'); ?></h3>
-            <p><?php print $theView->version; ?></p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-12 mb-3">
-            <h3 class="my-3"><?php $theView->write('HL_HELP_LICENCE'); ?></h3>
-            <?php print nl2br($theView->escapeVal($licence)); ?>
-        </div>
+    
+    <div class="m-3">
+        <h3 class="pt-2 fs-1"><?php $theView->write('SYSTEM_HL_OPTIONS_GENERAL'); ?></h3>
+        <?php print $content; ?>
+        
+        <h3 class="pt-5 fs-1"><?php $theView->write('VERSION'); ?></h3>
+        <p><?php print $theView->version; ?></p>
+
+        <h3 class="pt-5 fs-1"><?php $theView->write('HL_HELP_LICENCE'); ?></h3>
+        <?php print nl2br($theView->escapeVal($licence)); ?>
     </div>
 </div>
