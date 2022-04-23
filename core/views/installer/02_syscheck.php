210353266e850a53c60591189eaa6c56778658d5
stefan <stefan@stefan-pc>
2022-04-23 15:40:30 +0200

    style bugfix syscheck in installer

--- core/views/installer/02_syscheck.php
+++ core/views/installer/02_syscheck.php
@@ -1,5 +1,5 @@
 <?php /* @var $theView \fpcm\view\viewVars */ ?>
-<div class="row justify-content-center">
+<div class="row g-0 justify-content-center">
     <div class="col-12">
         <?php include $theView->getIncludePath('system/syscheck.php'); ?>        
     </div>
