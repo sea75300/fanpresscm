f38c096acc3179422cc1c3ef97b4fc1706ef4777
stefan <stefan@stefan-tuxedo>
2022-12-21 21:37:15 +0100

    restyling installer

--- core/views/installer/07_finalize.php
+++ core/views/installer/07_finalize.php
@@ -1,11 +1,9 @@
 <?php /* @var $theView fpcm\view\viewVars */ ?>
-<div class="row p-2 justify-content-center">
-    <div class="col-8 text-center">
-        <p><?php $theView->write('INSTALLER_FINALIZE_TEXT'); ?></p>
-        <?php if ($disableInstallerMsg) : ?>
-        <p class="text-danger"><?php $theView->write('INSTALLER_FINALIZE_DIABLED'); ?></p>
-        <?php endif; ?>
+<p class="col-8 text-center">
+    <p><?php $theView->write('INSTALLER_FINALIZE_TEXT'); ?></p>
+    <?php if ($disableInstallerMsg) : ?>
+    <p class="text-danger"><?php $theView->write('INSTALLER_FINALIZE_DIABLED'); ?></p>
+    <?php endif; ?>
 
-        <?php $theView->linkButton('toLogin')->setUrl($theView->controllerLink('system/login'))->setText('LOGIN_BTN')->setIcon('sign-in-alt'); ?>
-    </div>
-</div>
+    <?php $theView->linkButton('toLogin')->setUrl($theView->controllerLink('system/login'))->setText('LOGIN_BTN')->setIcon('sign-in-alt'); ?>
+</p>
