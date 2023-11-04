f3a4ecf6dcf23cfc319fc8b7eec50c60bdd59c73
stefan <stefan@stefan-pc>
2023-11-04 11:18:28 +0100

    Dark mode https://github.com/sea75300/fanpresscm/issues/18 Code cleanup

--- inc/view/view.php
+++ inc/view/view.php
@@ -98,6 +98,13 @@ class view {
     protected $jsFilesLocal = [];
 
     /**
+     * Javascript ECMA-modules
+     * @var array
+     * @since 5.2.0
+     */
+    protected $jsModuleFiles = [];
+
+    /**
      * View CSS files
      * @var array
      */
@@ -462,22 +469,21 @@ class view {
      */
     public function overrideJsLangVars(array $jsVars)
     {
-        $keys = array_values($jsVars);
-        $values = array_map([$this->language, 'translate'], array_values($jsVars));
-
-        $this->jsLangVars = array_combine($keys, $values);
+        $this->addJsLangVars($jsVars, true);
     }
 
     /**
      * Add new JS language vars
      * @param mixed $jsVars
      */
-    public function addJsLangVars(array $jsVars)
+    public function addJsLangVars(array $jsVars, bool $override = false)
     {
         $keys = array_values($jsVars);
         $values = array_map([$this->language, 'translate'], array_values($jsVars));
 
-        $this->jsLangVars = array_merge($this->jsLangVars, array_combine($keys, $values));
+        $this->jsLangVars = $override
+                          ? array_combine($keys, $values)
+                          : array_merge($this->jsLangVars, array_combine($keys, $values));
     }
 
     /**
