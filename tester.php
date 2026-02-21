ca3bb5f073eff60eeffc883d514cad1c39f555bd
stefan <stefan@stefan-tuxedo>
2026-02-21 20:41:19 +0100

    catch api exception in tester.php script

--- tester.php
+++ tester.php
@@ -21,15 +21,17 @@
             <h2>News</h2>
             <?php $api->showArticles(); ?>
         </div>
-        
+
         <div class="col-12  col-lg-8 my-2 mr-md-1  p-0 content background-white-trans border-top-orange-bold">
             <h2>Module</h2>
             <?php
-            $api->getModuleApi('nkorg/calendar')->display();
-
-            $api->getModuleApi('nkorg/extstats')->countAll();
-
-            $api->getModuleApi('nkorg/polls')->displayPoll();
+                try {
+                    $api->getModuleApi('nkorg/calendar')->display();
+                    $api->getModuleApi('nkorg/extstats')->countAll();
+                    $api->getModuleApi('nkorg/polls')->displayPoll();
+                } catch (Exception $exc) {
+                    echo $exc->getMessage();
+                }
             ?>
         </div>
     </body>
